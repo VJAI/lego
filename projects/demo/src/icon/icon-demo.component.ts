cc541a78e53cfcf7cdd9b6fe75b59b750c21f8e6
Vijaya Anand <vanand@apttus.com>
2019-09-21 08:14:22 +0530

    Updated Logo

--- projects/demo/src/icon/icon-demo.component.ts
+++ projects/demo/src/icon/icon-demo.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DemoService } from '../demo.service';
 
 @Component({
   selector: 'demo-icon',
-  templateUrl: './icon.html'
+  templateUrl: './icon.html',
+  styleUrls: ['./_icon.scss']
 })
-export class IconDemoComponent {
+export class IconDemoComponent implements OnInit {
+
+  constructor(private demoService: DemoService) { }
+
+  public icons: Array<string> = [];
+
+  ngOnInit(): void {
+    this.demoService.getTableData('icons.json').subscribe((res: Array<string>) => (this.icons = res));
+  }
+
+  public copy(event: UIEvent) {
+    const span = (<any>event.target).querySelector('span');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(span);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges();
+    alert(`${span.textContent} is copied to clipboard!`);
+  }
 }
